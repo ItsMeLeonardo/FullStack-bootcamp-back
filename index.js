ef57de8ecbc96062f52f51fe9385c2bcbc147c4b
Carlos Leonardao <74982565+ItsMeLeonardo@users.noreply.github.com>
2021-11-22 18:08:18 -0500

    [ADD] ⭕ some endpoints

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+app.use(express.json());
 
 let notes = [
   {
@@ -27,12 +29,54 @@ let notes = [
   },
 ];
 
-const app = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(notes));
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+// url with dynamic path => /:dynamicValue
+app.get("/api/notes/:id", (request, response) => {
+  const { id } = request.params;
+  const note = notes.find((note) => note.id === Number(id));
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const { id } = request.params;
+  notes = notes.filter((note) => note.id !== Number(id));
+
+  response.status(204).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const note = request.body;
+  if (!note?.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const newNote = {
+    content: note.content,
+    important: note.important || false,
+    date: new Date(),
+    id: notes.length + 1,
+  };
+
+  notes = notes.concat(newNote);
+
+  response.status(201).json(newNote);
 });
 
 const PORT = 3001;
-app.listen(PORT);
-
-console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
