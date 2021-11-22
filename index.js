2fa56cfe555995f5cc7d4a37769c17f5525ea499
Carlos Leonardao <74982565+ItsMeLeonardo@users.noreply.github.com>
2021-11-22 13:56:13 -0500

    init project with node

---
+++ index.js
@@ -0,0 +1,38 @@
+const http = require("http");
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+  {
+    id: 4,
+    content: "in react when the state change the entire components is rendered",
+    date: "2019-05-30T19:20:14.298Z",
+    important: false,
+  },
+];
+
+const app = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(notes));
+});
+
+const PORT = 3001;
+app.listen(PORT);
+
+console.log(`Server running at http://localhost:${PORT}/`);
